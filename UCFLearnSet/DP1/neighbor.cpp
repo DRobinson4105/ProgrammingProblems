2c3c72f91ad7d38f7142620f8895fba8fe9fea14
DRobinson4105 <drobinson4105@gmail.com>
2024-02-22 11:50:03 -0500

    Update neighbor.cpp

--- UCFLearnSet/DP1/neighbor.cpp
+++ UCFLearnSet/DP1/neighbor.cpp
@@ -4,24 +4,18 @@ using namespace std;
 unordered_map<int, pair<int, string>> dp;
 
 pair<int, string> solve(vector<int> houses, int curr, int k) {
-    // cout << "1" << endl;
     if (curr + 2 >= k) {
         if (curr < k) return {houses[curr], to_string(curr)};
         return {0, ""};
     }
-    // cout << "2" << endl;
+
     if (dp.count(curr)) return dp[curr];
-    // cout << "3" << endl;
+
     pair<int, string> left = solve(houses, curr + 2, k);
     pair<int, string> right = solve(houses, curr + 3, k);
 
-    if (left.first >= right.first) {
-        // cout << curr << " " << to_string(curr) + " " + left.second << endl;
-        return dp[curr] = {left.first + houses[curr], to_string(curr) + " " + left.second};
-    } else {
-        // cout << curr << " " << to_string(curr) + " " + right.second << endl;
-        return dp[curr] = {right.first + houses[curr], to_string(curr) + " " + right.second};
-    }
+    if (right.first > left.first) left = right;
+    return dp[curr] = {left.first + houses[curr], to_string(curr) + " " + left.second};
 }
 
 int main() {
@@ -37,8 +31,8 @@ int main() {
         pair<int, string> second = solve(houses, 1, k);
 
         cout << "Case #" << test << ": ";
-        if (first.first >= second.first) cout << first.first << " : " << first.second << endl;
-        else cout << second.first << " : " << second.second << endl;
+        if (second.first > first.first) first = second;
+        cout << first.first << " : " << first.second << endl;
         dp.clear();
     }
 
